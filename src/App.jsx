3c522eac3bca64537b41ba60632fccb429a523f3
Baishali Chatterjee <142675179+Baishalichatterjee@users.noreply.github.com>
2024-08-17 17:43:23 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Todos from './components/Todos';
+ import Todo from './components/todo';
+
+
+function App() {
+  return (
+    <>
+    <Todo/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+
+const Todo = () =>{
+    const [initial , setInitial] =useState();
+    const [data,setData] = useState([]);
+    const getInput = (event) =>{
+      console.log(event.target.value);
+      setInitial(event.target.value);
+    }
+    const getData = () =>{
+     console.log(initial);
+     let store = [...data,initial]
+     setData(store);
+     setInitial("")
+    }
+    const deleteTask=(index)=>{
+      let filterData = data.filter((currVal,indx)=>{
+        return index != indx 
+      })
+      setData(filterData)
+    }
+  return (
+   <>
+   <div className="container">
+    <div className="inputTask">
+      <input type="text" placeholder="Enter your Task"  value={initial} onChange={getInput} />
+      <button onClick={getData}>Add</button>
+    </div>
+    {data.map((curVal,index)=>{
+      return(
+        <>
+        <div className="taskData">
+          <p>{curVal}</p>
+          <i  id="deleteIcon" onClick={()=>deleteTask(index)}    class="fa-solid fa-trash"></i>
+        </div>
+        </>
+      )
+    })}
+   </div>
+   </>
+  );
+}
+export default Todo
